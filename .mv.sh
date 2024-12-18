37a21b362e6f1cf3db7df68058ea5cc42adcc798
Hyj18816828459 <heyujie0711@outlook.com>
2024-12-18 20:58:57 +0800

    move

--- .mv.sh
+++ .mv.sh
@@ -1,7 +1,7 @@
 mv ~/Downloads/*.zip .
 # 遍历每个 .zip 文件并解压
 for zipfile in ./*.zip; do
-    unzip "$zipfile" -d "${zipfile%.*}"  # 解压到以文件名命名的目录
+    unzip "${zipfile%.*}"  # 解压到以文件名命名的目录
 done
 rm ./*.zip
 
