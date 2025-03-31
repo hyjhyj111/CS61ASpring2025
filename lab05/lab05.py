9f91e2cae8b3dc848e6a51039ce149dddea39fae
Hyj18816828459 <heyujie0711@outlook.com>
2025-03-31 15:32:09 +0800

    latest

--- lab05/lab05.py
+++ lab05/lab05.py
@@ -27,6 +27,12 @@ def insert_items(s, before, after):
     True
     """
     "*** YOUR CODE HERE ***"
+    i = len(s)
+    while i >= 1:
+        if s[i - 1] == before:
+            s.insert(i, after)
+        i -= 1
+    return s
 
 
 def group_by(s, fn):
@@ -40,12 +46,12 @@ def group_by(s, fn):
     {9: [-3, 3], 4: [-2, 2], 1: [-1, 1], 0: [0]}
     """
     grouped = {}
-    for ____ in ____:
-        key = ____
+    for x in s:
+        key = fn(x)
         if key in grouped:
-            ____
+            grouped[key].append(x)
         else:
-            grouped[key] = ____
+            grouped[key] = [x]
     return grouped
 
 
@@ -71,6 +77,10 @@ def count_occurrences(t, n, x):
     2
     """
     "*** YOUR CODE HERE ***"
+    cnt = 0
+    for _ in range(n):
+        cnt += next(t) == x
+    return cnt
 
 
 def repeated(t, k):
@@ -94,6 +104,16 @@ def repeated(t, k):
     """
     assert k > 1
     "*** YOUR CODE HERE ***"
+    cur, cnt = 0, 0
+    for x in t:
+        if x == cur:
+            cnt += 1
+        else :
+            cur = x
+            cnt = 1
+        if cnt == k:
+            return cur
+
 
 
 def sprout_leaves(t, leaves):
@@ -130,6 +150,9 @@ def sprout_leaves(t, leaves):
           2
     """
     "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return tree(label(t), [tree(x) for x in leaves])
+    return tree(label(t), [sprout_leaves(x, leaves) for x in branches(t)])
 
 
 def partial_reverse(s, start):
@@ -145,6 +168,15 @@ def partial_reverse(s, start):
     [1, 2, 7, 6, 5, 3, 4]
     """
     "*** YOUR CODE HERE ***"
+    for i in range(len(s)):
+        if s[i] ==  start:
+            i += 1
+            j = len(s) - 1
+            while i < j:
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+            break
 
 
 
