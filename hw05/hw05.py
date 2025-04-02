a4217175689a017722d76389ba6c9303f23a1517
Hyj18816828459 <heyujie0711@outlook.com>
2025-04-02 18:27:00 +0800

    latest

--- hw05/hw05.py
+++ hw05/hw05.py
@@ -10,6 +10,15 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    yield n
+    if n == 1:
+        yield from hailstone(1)
+    else :
+        if n & 1:
+            yield from hailstone(n * 3 + 1)
+        else:
+            yield from hailstone(n // 2)
+
 
 
 def merge(a, b):
@@ -31,10 +40,17 @@ def merge(a, b):
     while True:
         if a_val == b_val:
             "*** YOUR CODE HERE ***"
+            yield a_val
+            a_val, b_val = next(a), next(b)
         elif a_val < b_val:
             "*** YOUR CODE HERE ***"
+            yield a_val
+            a_val = next(a)
         else:
             "*** YOUR CODE HERE ***"
+            yield b_val
+            b_val = next(b)
+
 
 
 def stair_ways(n):
@@ -51,6 +67,14 @@ def stair_ways(n):
     []
     """
     "*** YOUR CODE HERE ***"
+    def f(x, path):
+        if x == 0:
+            yield path
+        if x >= 1:
+            yield from f(x - 1, path + [1])
+        if x >= 2:
+            yield from f(x - 2, path + [2])
+    yield from(f(n, []))
 
 
 def yield_paths(t, value):
@@ -89,10 +113,10 @@ def yield_paths(t, value):
     [[0, 2], [0, 2, 1, 2]]
     """
     if label(t) == value:
-        yield ____
+        yield [label(t)]
     for b in branches(t):
-        for ____ in ____:
-            yield ____
+        for path in yield_paths(b, value):
+            yield [label(t)] + path
 
 
 
