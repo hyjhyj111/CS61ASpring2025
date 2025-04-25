2167a684219c038d2ee185d3809365ce3582e5f3
Hyj18816828459 <heyujie0711@outlook.com>
2025-04-25 11:16:20 +0800

    lab07

--- lab07/lab07.py
+++ lab07/lab07.py
@@ -40,6 +40,12 @@ class Account:
         """Return the number of years until balance would grow to amount."""
         assert self.balance > 0 and amount > 0 and self.interest > 0
         "*** YOUR CODE HERE ***"
+        cur = self.max_withdrawal
+        cnt = 0
+        while cur < amount:
+            cur = cur * (1 + self.interest)
+            cnt += 1
+        return cnt
 
 
 class FreeChecking(Account):
@@ -70,6 +76,12 @@ class FreeChecking(Account):
     free_withdrawals = 2
 
     "*** YOUR CODE HERE ***"
+    def withdraw(self, amount):
+        if not self.free_withdrawals:
+            amount += self.withdraw_fee
+        else:
+            self.free_withdrawals -= 1
+        return super().withdraw(amount)
 
 
 def without(s, i):
